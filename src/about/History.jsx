73459c3709b2889d9211de5c34e4902003b6da7c
soniayushi <ayushisoni2107@gmail.com>
2026-01-05 16:13:19 +0530

    history added to about page

---
+++ src/about/History.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function History(){
+    return(
+    <>
+         <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Oras posuere purus ut leo laculis convallis. 
+            Nullam fermentum odio quis volutpat liber sodales elementum. 
+            Pellentesque volutpat libero eu ex cursus lacinia, volutpat libero. 
+            Etiam vel consectetur dolor. 
+            <strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong> 
+            Oras posuere purus ut leo laculis convallis. Nullam fermentum odio quis.
+        </p>
+    </>
+    );
+}
+export default History;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,11 @@
 import Navbar from "../Components/Navbar";
-
+import History from "../about/History";
 
 function About() {
   return (
     <>
       <Navbar />
+      <History />
       </>
       );
     }
