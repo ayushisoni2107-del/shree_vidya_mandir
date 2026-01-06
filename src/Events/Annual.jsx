401e8c6a025f4a81a0b2dad1614444f52b4f6ee3
soniayushi <ayushisoni2107@gmail.com>
2026-01-06 20:07:44 +0530

    annual file updated

--- src/Events/Annual.jsx
+++ src/Events/Annual.jsx
@@ -11,6 +11,72 @@ function Annual(){
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc placerat, ante eu cursus pretium
         </p>
       </div>
+  <div className="events-grid">
+  <div className="event-card">
+    <img src="event4.png" />
+    <div className="overlay-text">
+      <h4>Annual Function</h4>
+      <p>Lorem ipsum dolor sit amet</p>
+    </div>
+  </div>
+
+  <div className="event-card">
+    <img src="event2.png" />
+    <div className="overlay-text">
+      <h4>Annual Function</h4>
+      <p>Lorem ipsum dolor sit amet</p>
+    </div>
+  </div>
+
+  <div className="event-card">
+    <img src="event1.png" />
+    <div className="overlay-text">
+      <h4>Annual Function</h4>
+      <p>Lorem ipsum dolor sit amet</p>
+    </div>
+  </div>
+
+  <div className="event-card">
+    <img src="event3.png" />
+    <div className="overlay-text">
+      <h4>Annual Function</h4>
+      <p>Lorem ipsum dolor sit amet</p>
+    </div>
+  </div>
+
+  <div className="event-card">
+    <img src="event1.png" />
+    <div className="overlay-text">
+      <h4>Annual Function</h4>
+      <p>Lorem ipsum dolor sit amet</p>
+    </div>
+  </div>
+
+  <div className="event-card">
+    <img src="event3.png" />
+    <div className="overlay-text">
+      <h4>Annual Function</h4>
+      <p>Lorem ipsum dolor sit amet</p>
+    </div>
+  </div>
+
+  <div className="event-card">
+    <img src="event4.png" />
+    <div className="overlay-text">
+      <h4>Annual Function</h4>
+      <p>Lorem ipsum dolor sit amet</p>
+    </div>
+  </div>
+
+  <div className="event-card">
+    <img src="event2.png" />
+    <div className="overlay-text">
+      <h4>Annual Function</h4>
+      <p>Lorem ipsum dolor sit amet</p>
+    </div>
+  </div>
+</div>
+
         </>
     );
 }
